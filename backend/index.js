125bed6b78bc20fc53eabbca535ac8b3cfc6012b
FacundoVillarroel <facu.villarroel96@gmail.com>
2025-03-03 21:10:26 -0300

    [DB] - connection and config DB

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
 dotenv.config();
+
+const connectDB = require("./src/config/db");
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+//DB connection
+connectDB()
+
+//middlewares
 app.use(express.json());
 
 app.listen(PORT, () => {
---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.error("Failed to connect to DB", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
