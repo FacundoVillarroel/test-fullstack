7381734d37464ad10fc214d176e0ffa854dd6a81
FacundoVillarroel <facu.villarroel96@gmail.com>
2025-03-03 23:02:41 -0300

    [feature] - implemented delete product from db with delete button in ListItem.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,32 +4,56 @@ import ProductList from "../../components/productList/ProductList";
 import { Title } from "./home.styles";
 
 const SERVER_URI = "http://localhost:8080";
+
 const Home = () => {
   const [products, setProducts] = useState([]);
   const [editProduct, setEditProduct] = useState(null);
 
+  //function to Fetch product from DB
   const fetchProducts = async () => {
     try {
       const response = await fetch(`${SERVER_URI}/api/products`);
       if (!response.ok) {
-        window.alert("Error fetching the product");
+        window.alert("Error fetching products");
         throw new Error("Error fetching products");
       }
       const productsList = await response.json();
       setProducts(productsList);
     } catch (error) {
+      window.alert("Error fetching the product");
       console.error("Error fetching products");
     }
   };
 
+  //initially fetch all products in DB
   useEffect(() => {
     fetchProducts();
   }, []);
 
-  const onDelete = (id) => {
-    console.log("Delete Product", id);
+  //function to delete product on DB by its ID
+  const onDelete = async (id) => {
+    try {
+      const response = await fetch(`${SERVER_URI}/api/products/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        window.alert("Error deleting products");
+        throw new Error("Error deleting products");
+      }
+      const data = await response.json();
+
+      const newProductsList = products.filter(
+        (product) => product._id !== data.productDeleted._id
+      );
+      window.alert("Product deleted successfully!");
+      setProducts(newProductsList);
+    } catch (error) {
+      window.alert("Error deleting the product");
+      console.error("Error deleting product");
+    }
   };
 
+  //Send POST request to DB to create a product
   const onSubmit = async (formData) => {
     try {
       const response = await fetch(`${SERVER_URI}/api/products`, {
@@ -44,9 +68,10 @@ const Home = () => {
         throw new Error("Error creating product");
       }
       const productCreated = await response.json();
-      window.alert("Product created successfully")
+      window.alert("Product created successfully");
       setProducts((prevProducts) => [...prevProducts, productCreated]);
     } catch (error) {
+      window.alert("Error creating the product");
       console.error("Error submitting form");
     }
   };
