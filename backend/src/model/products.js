7c6670f84410a0e299649971192f4e4d6206884e
FacundoVillarroel <facu.villarroel96@gmail.com>
2025-03-03 21:03:04 -0300

    [model] - creating model for products

---
+++ backend/src/model/products.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Product = new mongoose.Model("Product", ProductSchema);
+
+module.exports = Product;
